9e4efa15630e9c0db4df8a9161794e7c07b9b361
DgLoo <srcqfestu@gmail.com>
2025-03-06 17:39:01 +0800

    Add files via upload

---
+++ SimpleScoreboard/lua/autorun/client/cl_Simple_Scoreboard_languages.lua
@@ -0,0 +1,45 @@
+-- Insert or update translation entries.
+SIMPLE_SCOREBOARD_LANGUAGES = {
+    ["player_list"] = {
+        ["en"] = "Player List",
+        ["zh-CN"] = "玩家列表",
+        ["zh-TW"] = "玩家清單",
+        ["ru"] = "Список игроков",
+        ["ja"] = "プレイヤー一覧",
+        ["ko"] = "플레이어 목록"
+    },
+    ["columns"] = {
+        name = { 
+            ["en"] = "Name",
+            ["zh-CN"] = "名称",
+            ["zh-TW"] = "名稱",
+            ["ru"] = "Имя",
+            ["ja"] = "名前",
+            ["ko"] = "이름"
+        },
+        kills = {
+            ["en"] = "K",
+            ["zh-CN"] = "击杀",
+            ["zh-TW"] = "擊殺", 
+            ["ru"] = "Фраг",
+            ["ja"] = "キル",
+            ["ko"] = "킬"
+        },
+        death = {
+            ["en"] = "D",
+            ["zh-CN"] = "死亡",
+            ["zh-TW"] = "死亡", 
+            ["ru"] = "смерть",
+            ["ja"] = "デス数",
+            ["ko"] = "죽음"
+        },
+    },
+    ["voice_tooltip"] = {
+        ["en"] = "Toggle voice",
+        ["zh-CN"] = "切换语音",
+        ["zh-TW"] = "切換語音",
+        ["ru"] = "Переключить голос",
+        ["ja"] = "ボイスを切り替え",
+        ["ko"] = "음성 전환"
+    }
+}
